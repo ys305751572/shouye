a94ec7c2717ab5e557bc6074960bac74156a0bcf
ys305751572 <305751572@qq.com>
2016-10-18 21:50:31 +0800

    1

--- src/test/java/test/RoleServiceTest.java
+++ src/test/java/test/RoleServiceTest.java
@@ -1,24 +1,24 @@
-package test;
-
-import com.smallchill.system.service.RoleService;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-
-
-/**
- *
- * Created by Administrator on 2016/10/17 0017.
- */
-public class RoleServiceTest extends BaseJunit4Test{
-
-    @Autowired
-    private RoleService roleService;
-
-    @Test
-    @Transactional
-    public void test() {
-
-        System.out.println("============test=============" + roleService.findAll().size());
-    }
-}
+//package test;
+//
+//import com.smallchill.system.service.RoleService;
+//import org.junit.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//
+///**
+// *
+// * Created by Administrator on 2016/10/17 0017.
+// */
+//public class RoleServiceTest extends BaseJunit4Test{
+//
+//    @Autowired
+//    private RoleService roleService;
+//
+//    @Test
+//    @Transactional
+//    public void test() {
+//
+//        System.out.println("============test=============" + roleService.findAll().size());
+//    }
+//}
