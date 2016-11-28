b091622505599f04337103fa64966c80446ca060
ys305751572 <305751572@qq.com>
2016-11-28 16:52:07 +0800

    1

--- src/main/java/com/smallchill/core/toolbox/kit/CommonKit.java
+++ src/main/java/com/smallchill/core/toolbox/kit/CommonKit.java
@@ -1,5 +1,6 @@
 package com.smallchill.core.toolbox.kit;
 
+import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -10,5 +11,15 @@ public class CommonKit {
 
     public static String getNo() {
         return UUID.randomUUID().toString().replace("-","");
+    }
+
+    /**
+     * 生成订单号
+     */
+    public static String generateSn() {
+        Random r = new Random();
+        int random = r.nextInt(1000);
+        long current = System.currentTimeMillis();
+        return String.valueOf(current) + String.valueOf(random);
     }
 }
